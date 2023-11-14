451e73a73c6c8706608f6e0253f675aa57088f0c
abhinav0git <abhinav0c@gmail.com>
2023-11-14 13:42:39 +0530

    updated index,script for drag and drop, udpated some style elements

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,25 +1,45 @@
-// script.js
-document.getElementById('file').addEventListener('change', function () 
-{
+//new script
+
+document.addEventListener('DOMContentLoaded', function () {
     const fileInput = document.getElementById('file');
     const imagePreview = document.getElementById('image');
-    const imageContainer = document.getElementById('something');
+    const imageContainer = document.getElementById('something');  //after image jahan par show hogi
+    // const svgImage = document.getElementById('svgImage');
 
-    if (fileInput.files.length > 0) {
+    // drag and drop events
+    const dragArea = document.querySelector('.uploadsection');
+
+    dragArea.addEventListener('dragover', function (e) {
+        e.preventDefault();
+        dragArea.classList.add('drag-over');
+    });
+
+    dragArea.addEventListener('dragleave', function () {
+        dragArea.classList.remove('drag-over');
+    });
+
+    dragArea.addEventListener('drop', function (e) {
+        e.preventDefault();
+        dragArea.classList.remove('drag-over');
+
+        const droppedFiles = e.dataTransfer.files;
+        fileInput.files = droppedFiles;
+
+        handleFileSelect();
+    });
+
+    fileInput.addEventListener('change', handleFileSelect);
+
+    function handleFileSelect() {
         const file = fileInput.files[0];
-        const reader = new FileReader();
+        const reader = new FileReader();    // to read the contents of the selected image file (specified by fileInput.files[0]) asynchronously
 
         reader.onload = function () {
             imagePreview.src = reader.result;
             imageContainer.style.display = 'block';
             svgImage.style.display = 'none'; // Hide the SVG image
-        }
+        };
 
         reader.readAsDataURL(file);
-    } 
-    else {
-        imagePreview.src = '';
-        imageContainer.style.display = 'none';
-        svgImage.style.display = 'block'; // Show the SVG image
     }
 });
